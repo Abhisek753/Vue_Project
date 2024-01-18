9978280d4cfa40cb6dc59dea05faf9761e9cacaf
Abhisek753 <properachievement@gmail.com>
2024-01-18 13:42:43 +0530

    Akansha

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="child">
+    <p>Prop1: {{ prop1 }}</p>
+    <p>Prop2: {{ prop2 }}</p>
+    <p>Prop3: {{ prop3.object }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    prop1: String,
+    prop2: Number,
+    prop3: Object,
+  },
+};
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="parent">
+    <TheWelcome :prop1="value1" :prop2="value2" :prop3="value3" />
+  </div>
+</template>
+
+<script>
+import TheWelcome from '../components/TheWelcome.vue';
+
+export default {
+  components: {
+    TheWelcome,
+  },
+  data() {
+    return {
+      value1: 'Data for prop1',
+      value2: 42,
+      value3: { object: 'property' },
+    };
+  },
+};
+</script>
+
